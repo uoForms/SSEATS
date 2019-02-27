c3be985ad7ff212d87c7d5b0ba6dce285eba64e1
DavidBonsant <bonsantdavid@gmail.com>
2019-02-27 18:53:55 -0500

    cleaned up login and set default to 404. didn't copy the css

--- cws-react-node/src/login.jsx
+++ cws-react-node/src/login.jsx
@@ -4,14 +4,10 @@ export default class Login extends React.Component {
   render() {
     return (
       <form>
-        <input type="text" name="firstname" value="Mickey" /><br/>
-        <input type="password" name="lastname" value="Mouse" /><br/>
+        <input type="text" name="firstname" /><br/>
+        <input type="password" name="lastname" /><br/>
         <input type="submit" value="Submit" />
       </form> 
     );
   }
 }
-
-// ========================================
-
-//ReactDOM.render(<Page />, document.getElementById("root"));
