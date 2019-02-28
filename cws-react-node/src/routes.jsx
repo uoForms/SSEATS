2c877824ce875104b36b56ebecf375347dd0909e
Christian Savoie <christiansavoie16@gmail.com>
2019-02-27 19:11:28 -0500

    Added 404 to routing instead of default.

--- cws-react-node/src/routes.jsx
+++ cws-react-node/src/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // pages
 import Login from './login.jsx';
-import Default from './default.jsx'
+import NotFound from './404.jsx'
 
 export default class Routes extends React.Component {
   render() {
@@ -10,7 +10,7 @@ export default class Routes extends React.Component {
         <BrowserRouter>
           <Switch>
             <Route exact path='/' component={Login} />
-            <Route path='/*' component={Default} />
+            <Route path='/*' component={NotFound} />
           </Switch>
         </BrowserRouter>
     );
