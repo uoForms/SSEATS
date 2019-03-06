4a527135c602b59e6208766ab5c767c729ac1d07
DavidBonsant <bonsantdavid@gmail.com>
2019-03-06 10:58:07 -0500

    client side login page and css

--- cws-react-node/src/login.jsx
+++ cws-react-node/src/login.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
+import './form.css';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      username:'',
+      password:'',
+      showError:false,
+      errorMessage:''
+    };
+  }
+
+  handleClick(event) {
+    // Defaults to incomplete credentials.
+    let wrong = true;
+    let msg = 'You must enter username and password.';
+    // Simulate a invalid login.
+    if (this.state.username != '' && this.state.password != '') {
+      msg = "Invalid username or password.";
+    }
+    // Simulate a valid login.
+    if (this.state.username == 'admin' && this.state.password == 'admin') {
+      wrong = false;
+      window.location.href = '/home';
+    }
+    this.setState({showError:wrong, errorMessage:msg});
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" name="firstname" /><br/>
-        <input type="password" name="lastname" /><br/>
-        <input type="submit" value="Submit" />
-      </form> 
+      <div id="textForm">
+        {this.state.showError ? <h5 id="loginError">{this.state.errorMessage}</h5> : null}
+        <input 
+          id="textfield1"
+          type="text"  
+          placeholder="Username" 
+          title="Enter your username." 
+          onChange={() => this.setState({username:document.getElementById('textfield1').value})}
+        /><br/>
+        <input  
+          id="textfield2"
+          type="password" 
+          placeholder="Password" 
+          title="Enter your password." 
+          onChange={() => this.setState({password:document.getElementById('textfield2').value})}
+        /><br/>
+        <button id="textfield3" title="Login to your account." onClick={(event)=>this.handleClick(event)}>Login</button>
+      </div>
     );
   }
 }
