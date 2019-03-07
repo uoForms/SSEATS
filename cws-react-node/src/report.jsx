16749872a4f283f7b7ac3f7f43efd0a22e88008f
luanphung <lphun032@uottawa.ca>
2019-03-07 12:39:27 -0500

    Fixed CircleCi Error

--- cws-react-node/src/report.jsx
+++ cws-react-node/src/report.jsx
@@ -1,8 +1,3 @@
-import React, { Component } from 'react';
-import Header from './header.jsx'
-import Routes from './routes.jsx'
-
-
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
