477463d0e296ae6365c231962819fe0dec095caa
ChrisSavoie16 <christiansavoie16@gmail.com>
2019-03-15 19:33:36 -0400

    Authenticated routing (#39)
    * Verifies authentication status, but does not hold session.
    
    Sessions do not seem to hold through different requests, so only the non 
    authenticated routing is currently available.
    
    * Added a promise to wait the initialisation of firebase, then render the routes.

--- cws-react-node/src/routes.jsx
+++ cws-react-node/src/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { withFirebase } from './component/firebase/context'
 // pages
 import AuthLandingPage from './component/landingPage/authLandingPage.jsx';
 import NoAuthLandingPage from './component/landingPage/noAuthLandingPage.jsx';
@@ -8,18 +9,66 @@ import NotFound from './404.jsx'
 import Report from './report.jsx'
 
 
-export default class Routes extends React.Component {
-  render() {
+class RoutesBase extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isAuthenticating: true
+    };
+  }
+
+  resolveUser() {
+    return new Promise((resolve, reject) => {
+      this.props.firebase.auth.onAuthStateChanged((user) => {
+        resolve(user);
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.resolveUser().then((user) => {
+      this.setState({isAuthenticating: false});
+    });
+  }
+
+  unauthenticatedRouting() {
     return (
         <BrowserRouter>
           <Switch>
-            <Route exact path='/' component={AuthLandingPage} />
-            <Route exact path='/home' component={NoAuthLandingPage} />
+            <Route exact path='/' component={NoAuthLandingPage} />
             <Route exact path='/login' component={LoginPage} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+    );
+  }
+
+  authenticatedRouting() {
+    return (
+        <BrowserRouter>
+          <Switch>
+            {/* Since logged in, just redirect to home */}
+            <Route exact path="/login" render={() => (
+                <Redirect to="/"/>
+            )}/>
+            <Route exact path='/' component={AuthLandingPage} />
             <Route exact path='/report' component={Report}/>
             <Route component={NotFound} />
           </Switch>
         </BrowserRouter>
     );
   }
+
+  render() {
+    if(this.state.isAuthenticating){
+      return null;
+    } else {
+      return this.props.firebase.auth.currentUser === null ?
+        this.unauthenticatedRouting() : this.authenticatedRouting();
+    }
+  }
 }
+
+const Routes = withFirebase(RoutesBase);
+
+export default Routes;
