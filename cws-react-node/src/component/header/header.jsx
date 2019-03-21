b8e89545a6446fab2fa4288c5f993bb95b5d393b
Christian Savoie <christiansavoie16@gmail.com>
2019-03-20 22:43:19 -0400

    Dynamic header according to permissions Doing home page and routes next.

--- cws-react-node/src/component/header/header.jsx
+++ cws-react-node/src/component/header/header.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import { withFirebase } from '../firebase/context'
 
-export default class Header extends React.Component {
-  content = ""
+class HeaderBase extends React.Component {
+  filterPage(value){
+    console.log("32");
+    console.log(value);
+    return value.type !== null && value.type === 'page';
+  }
 
-  setContent(content) {
-    this.content = content
-  };
+  routingNavItems() {
+    let list = this.props.firebase.userPermissions
+      .filter(permission => permission.type !== null
+        && permission.type === 'page')
+      .map((page, i) => {
+        console.log(i)
+        return (
+          <Nav.Item key={i}>
+            <Nav.Link href={page.link}>{page.shortLabel}</Nav.Link>
+          </Nav.Item>
+        );
+      });
+    console.log(list);
+    return list;
+  }
+
+  userNavItems(){
+    let navItems = [];
+    if(this.props.firebase.auth.currentUser === null){
+      navItems.push(
+        <Nav.Item key='0'>
+          <Nav.Link href="/login">Login</Nav.Link>
+        </Nav.Item>
+      );
+    } else {
+      // TODO: When user session operations is complete, change this render
+      //       to have the logged in nav items.
+      navItems.push(
+        <Nav.Item key='0'>
+          <Nav.Link href="/login">Login</Nav.Link>
+        </Nav.Item>
+      );
+    }
+    return navItems;
+  }
+
 
   render() {
     return (
@@ -17,14 +55,16 @@ export default class Header extends React.Component {
             <Nav.Item>
               <Nav.Link href="/">Home</Nav.Link>
             </Nav.Item>
+            {this.routingNavItems()}
           </Nav>
           <Nav>
-            <Nav.Item>
-              <Nav.Link href="/login">Login</Nav.Link>
-            </Nav.Item>
+            {this.userNavItems()}
           </Nav>
         </Navbar>
 
-      );
+    );
   }
 }
+
+const Header = withFirebase(HeaderBase);
+export default Header;
--- cws-react-node/src/routes.jsx
+++ cws-react-node/src/routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { withFirebase } from './component/firebase/context'
+import { withFirebase } from './component/firebase/context';
 
 // pages
 import AuthLandingPage from './component/landingPage/authLandingPage.jsx';
@@ -12,38 +12,6 @@ import Report from './report.jsx'
 import manageRoles from './component/firebase/manageRoles.js'
 
 class RoutesBase extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      isAuthenticating: true
-    };
-  }
-
-  resolveUser() {
-    return new Promise((resolve, reject) => {
-      this.props.firebase.auth.onAuthStateChanged((user) => {
-        if (this.props.firebase.auth.currentUser!==null) {
-          // Fetch user permissions before page loads
-          this.props.firebase.db.collection('users').doc(this.props.firebase.auth.currentUser.uid)
-            .collection('permissions').get().then(permissions=>{
-              for (let i = 0; i < permissions.docs.length; i++){
-                this.props.firebase.userPermissions.push(permissions.docs[i].get('label'));
-              }
-              resolve(user);
-            });
-        } else {
-          resolve(user);
-        }
-      });
-    });
-  }
-
-  componentDidMount() {
-    this.resolveUser().then((user) => {
-      this.setState({isAuthenticating: false});
-    });
-  }
-
   unauthenticatedRouting() {
     return (
         <BrowserRouter>
@@ -73,18 +41,14 @@ class RoutesBase extends React.Component {
   }
 
   render() {
-    if(this.state.isAuthenticating){
-      return null;
-    } else {
-      // update permissions
-      if (this.props.firebase.auth.currentUser!==null) {
-        setTimeout(manageRoles.updateUserPermissions, 0, 
-          this.props.firebase.db.collection('users').doc(this.props.firebase.auth.currentUser.uid));
-      }
-      return this.props.firebase.auth.currentUser === null ?
-        this.unauthenticatedRouting() : this.authenticatedRouting();
+    // update permissions
+    if (this.props.firebase.auth.currentUser!==null) {
+      setTimeout(manageRoles.updateUserPermissions, 0,
+        this.props.firebase.db.collection('users').doc(this.props.firebase.auth.currentUser.uid));
     }
-  }
+    return this.props.firebase.auth.currentUser === null ?
+      this.unauthenticatedRouting() : this.authenticatedRouting();
+    }
 }
 
 const Routes = withFirebase(RoutesBase);
