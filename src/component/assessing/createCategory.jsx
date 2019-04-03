169b95cf4ff6f321b1edac19f409087bb47dfb67
Christian Savoie <christiansavoie16@gmail.com>
2019-04-03 17:45:56 -0400

    Added name field and dropdown in single row

--- src/component/assessing/createCategory.jsx
+++ src/component/assessing/createCategory.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { withFirebase } from '../firebase/context'
@@ -18,9 +19,12 @@ class CreateCategoryBase extends React.Component {
 
   getReportTypes(){
     this.props.firebase.db.collection('report_type').get().then((reports) => {
-      let reportOptionsArray = reports.docs.map((report, i) =>
-        <option key={i} value={report.ref.path}>{report.data().name}</option>
-      );
+      let reportOptionsArray = reports.docs.map((report, i) => {
+          if(i === 0){
+            this.setState({selectedType:report.ref.path});
+          }
+          return (<option key={i} value={report.ref.path}>{report.data().name}</option>);
+      });
       let reportMapping = {}
       reports.docs.forEach((report) => {
         reportMapping[report.ref.path] = report;
@@ -34,11 +38,36 @@ class CreateCategoryBase extends React.Component {
 
   render() {
     return (
-      <Card style={{ width: '50vw', minWidth: '10rem', margin: '5rem auto'}}>
+      <Card style={{ width: '60vw', minWidth: '10rem', margin: '5rem auto'}}>
         <Card.Body>
-          <Form.Group>
-            <Form.Label>Report Type</Form.Label>
-          </Form.Group>
+          <div className="h4">Create Category</div>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Category Name</Form.Label>
+                <Form.Control
+                  id = "categoryName"
+                  placeholder = "Enter New Category Name"
+                  title = "Category Name"
+                  onChange={_ => {
+                    this.setState({categoryName:document.getElementById('categoryName').value})
+                  }}
+                  >
+                </Form.Control>
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Report Type</Form.Label>
+                <Form.Control as="select"
+                  id = "reportType"
+                  placeholder = "Select a Report Type"
+                  title = "Report Type"
+                  onChange={_ => {
+                    this.setState({selectedType:document.getElementById('reportType').value})
+                  }}
+                  children = {this.state.reportOptions}
+                  >
+                </Form.Control>
+            </Form.Group>
+          </Form.Row>
         </Card.Body>
       </Card>
     );
