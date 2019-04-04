86ca3d45fc20961154b074b898fe375afedb53c3
luanphung <lphun032@uottawa.ca>
2019-04-04 12:12:44 -0400

    Unused Var Fixed

--- src/component/assessing/report.jsx
+++ src/component/assessing/report.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Sidebar from "react-sidebar";
-import Col from 'react-bootstrap/Col'
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
