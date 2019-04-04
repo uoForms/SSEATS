99717b0b58df17476cebd42569573a465cba556f
luanphung <lphun032@uottawa.ca>
2019-04-04 12:10:32 -0400

    Adding refresh after score added

--- src/component/assessing/report.jsx
+++ src/component/assessing/report.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Sidebar from "react-sidebar";
+import Col from 'react-bootstrap/Col'
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
@@ -111,38 +112,40 @@ class ReportBase extends React.Component {
       <div className="ag-theme-balham"
         style = {{flex:1, height:'80vh', width: '80%', margin: '2rem auto'}}
       >
-      <Form.Group>
-        <Form.Label>Select a Subject</Form.Label>
-        <Form.Control as="select"
-          id = "subject"
-          placeholder = "Select a Subject"
-          title = "Subject"
-          onChange={_ => {
-            this.handleChange(document.getElementById('subject').value)
-          }}
-          children = {this.state.subjects}
-          >
-        </Form.Control>
-      </Form.Group>
-      <Form.Group>
-        {
-          this.state.sidebarOpen?
-          <Sidebar
-            sidebar={<AddScore subjectDocumentReference={this.props.firebase.db.doc(this.state.subjectDocRef)} exit={_=>this.setState({sidebarOpen:false})}/>}
-            open={true}
-            children={<div/>}
-            onSetOpen={event=>this.onSetSidebarOpen(event, this)}
-            styles={{ sidebar: { background: "white" } }}
-            pullRight={true}
-          />
-          :null
-        }
-        {
-          this.state.subjectDocRef==="" ?
-          <Button variant="secondary" disabled>Add Score</Button> :
-          <Button onClick={_=>this.setState({sidebarOpen:true})}>Add Score</Button>
-        }
-      </Form.Group>
+        <Form.Group>
+        <Form.Label>Select a Subject:</Form.Label>
+          <Form.Control as="select"
+            id = "subject"
+            placeholder = "Select a Subject"
+            title = "Subject"
+            onChange={_ => {
+              this.handleChange(document.getElementById('subject').value)
+            }}
+            children = {this.state.subjects}
+            >
+          </Form.Control>
+        </Form.Group>
+        <Form.Group> 
+          {
+            this.state.sidebarOpen?
+            <Sidebar
+              sidebar={<AddScore subjectDocumentReference={this.props.firebase.db.doc(this.state.subjectDocRef)} exit={_=>{
+                this.handleChange(this.state.subjectDocRef)
+                this.setState({sidebarOpen:false})}}/>}
+              open={true}
+              children={<div/>}
+              onSetOpen={event=>this.onSetSidebarOpen(event, this)}
+              styles={{ sidebar: { background: "white" } }}
+              pullRight={true}
+            />
+            :null
+          }
+          {
+            this.state.subjectDocRef==="" ?
+            <Button variant="secondary" disabled>Add Score</Button> :
+            <Button onClick={_=>this.setState({sidebarOpen:true})}>Add Score</Button>
+          }
+        </Form.Group>
         <AgGridReact 
           style={{maxWidth:"100%"}}
           columnDefs = {this.state.columnDefs}
