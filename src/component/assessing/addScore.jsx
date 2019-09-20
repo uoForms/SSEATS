67341c7c1fffab9580b3e136638293bda0806525
DavidBonsant <bonsantdavid@gmail.com>
2019-09-20 16:56:08 -0400

    refactoring var to let.

--- src/component/assessing/addScore.jsx
+++ src/component/assessing/addScore.jsx
@@ -17,13 +17,13 @@ class AddScoreBase extends React.Component {
     };
     manageScore.getCriterias(this.props.firebase.db, this.props.selectedCategory).then(criteriaMap=>{
       let options = [];
-      for (var category in criteriaMap) {
+      for (let category in criteriaMap) {
         if (criteriaMap.hasOwnProperty(category)){
-          for(var feature in criteriaMap[category]) {
+          for(let feature in criteriaMap[category]) {
             if (criteriaMap[category].hasOwnProperty(feature)){
               let categoryOptions = [];
-              for (var i in criteriaMap[category][feature]) {
-                for(var criteria in criteriaMap[category][feature][i]) {
+              for (let i in criteriaMap[category][feature]) {
+                for(let criteria in criteriaMap[category][feature][i]) {
                   if (criteriaMap[category][feature][i].hasOwnProperty(criteria)){
                     categoryOptions.push((_=>{
                       this.setState({criteria:criteriaMap[category][feature][i][criteria]});
